8e73aac994858f86cfc4d3207d08adba1c4342ff
Emmanuel Odonkor <emmadonk@gmail.com>
2022-12-16 17:03:04 +0000

    still

--- users/processsearch.php
+++ users/processsearch.php
@@ -78,6 +78,16 @@ function ajaxsearch($shterm)
 			//execute sql
 			$result_2 = $conn->query($sql_2);
 
+			$sql_3 = "SELECT distinct year FROM dues WHERE memberid = '$memberid'";
+
+			//execute sql
+			$result_3 = $conn->query($sql_3);
+
+			$sql_4 = "SELECT memberid,dues_id,month,grouptype,amount,year,dateofpayment,adminid,approvedby FROM dues WHERE memberid = '$memberid' AND year = '$sql_3'";
+
+			//execute sql
+			$result_4 = $conn->query($sql_4);
+
 			echo "
 						<div class='modal fade' id='dues' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1'
 						aria-labelledby='staticBackdropLabel' aria-hidden='true'>
@@ -88,7 +98,7 @@ function ajaxsearch($shterm)
 								<button type='button' id='fclose_b' class='close' data-bs-dismiss='modal'>&times;</button>
 							</div>
 							<div class='modal-body'>
-								<div class='col-lg-12'>
+								<div class='col-lg-12 p-0'>
 								<div class='form-row'>
 									<!--Form Details-->
 									<div class='col-12'>
@@ -96,98 +106,139 @@ function ajaxsearch($shterm)
 										<div class='form-row mt-3'>
 			
 										<div class='col-12'>
-						
-											<div class='form-header' style='background-color: #003bb3; border-radius: 0.2rem'>
-												<h6 class='mt-2 text-center'>
-														Dues Payment Ordered By Year &nbsp; <i class='fa fa-calendar' style='color:white;' aria-hidden='true'></i>
-												</h6>
+											<div class='form-header modalhead' style='background-color: #003bb3; border-radius: 0.2rem'>
+												<h6 class='mt-2 text-center modaltext'>
+														Total Dues Payment&nbsp; <i class='fa fa-calendar' style='color:white;' aria-hidden='true'></i>
+												</h6>		
 											</div>
-							<!--table 2 Mobile View-->
-							<div class='table-responsive'>
-								<table class='table table-striped table-hover table-bordered'>
-									<thead>
-										<tr>
-											<th scope='col' class='text-center'></th>
-											<th scope='col' class='text-center'>Month</th>
-											<th scope='col' class='text-center'>Amount</th>
-											<th scope='col' class='text-center' >Year</th>
-										</tr>
-									</thead>
-									<tbody>";
 
-								foreach ($conn->query($sql_2) as $row_2) {
-									echo "
-									<tr class='accordion-toggle collapsed text-center' id='accordion1' data-bs-toggle='collapse' data-bs-target='#collapseOne-$row_2[dues_id]' aria-expanded='true' aria-controls='collapseOne'>
-										<td class='expand-button text-center'></td>
-										<td>$row_2[month]</td>
-										<td style='color:green;'>$row_2[amount].00</td>
-										<td>$row_2[year]</td>
-									</tr>";
-								
-
-								
-									echo "
-									<tr class='hide-table-padding m-auto'>
-										<!--<td></td>-->
-										<td colspan='4'>
-											<div id='collapseOne-$row_2[dues_id]' class='collapse in p-3'>
-												<div class='col-12'>
-													<div class='row' id='drop1' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
-														<label class='col-4' style='font-weight:bold;text-align: right;padding-left: 0;'>Dues ID:</label>
-														<label class='col-5' style='text-align: left;' >$row_2[dues_id]</label>
-													</div>
-													<div class='row' id='drop1' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
-														<label class='col-4' style='font-weight:bold;text-align: right;padding-left: 0;'>Date:</label>
-														<label class='col-5' style='text-align: left;' >$row_2[dateofpayment]</label>
-													</div>
-													<div class='row' id='drop2' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
-														<label  class='col-4' style='font-weight:bold;text-align: right;color:#003bb3;'>Admin ID:</label>
-														<label  class='col-5' style='text-align: left;color:#003bb3;'>$row_2[adminid]</label>
-													</div>
-													<div class='row' id='drop2' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
-														<label class='col-4' style='font-weight:bold;text-align: right;color:#003bb3;'>Admin:</label>
-														<label class='col-5' style='text-align: left;color:#003bb3;'>$row_2[approvedby]</label>
-													</div>
-													
+											<!--Choose Year -->
+											<div class='col-8 mt-3 p-0 chooseyear'>
+													<div class='form-group'>
+													<select id='funeralname' name='funeralname' placeholder='Choose Dues Payment Year' class='form-control'
+													required>
+													<option name='' value= style='display:none;'>Choose Dues Payment Year</option>";
+													foreach ($conn->query($sql_3) as $row){
+													echo "
+														<option value='$row[year]'>$row[year]</option>";
+													}
+													if(($result_3->num_rows == 0))
+														{
+															echo "
+															<option name='' value=''>No Payment Year Found</option>';
+															";        		
+														}
+													echo "</select>
 												</div>
 											</div>
-										</td>
-									</tr>";
-								}
-											//If Member does not have any Due payments made
-										if (($result_2->num_rows == 0)) {
-											echo '
-											<div class="col-12">
-												<div class="form-header" style="color: red; border-radius: 0.2rem">
-													<h6 class="mt-2 text-center">
-													No Record of Dues Payments&nbsp; <i class="fa fa-exclamation-triangle" style="color:red;" aria-hidden="true"></i>
-													</h6>
-												</div>
-											</div>';
+											<!--End of Choose Year -->
+										
+										
+
+										<!--table 2 Mobile View-->
+										<div class='table-responsive'>
+											<table class='table table-striped table-hover table-bordered'>
+												<thead class='table-heads'>
+													<tr>
+														<th scope='col' class='text-center'></th>
+														<th scope='col' class='text-center'>Month</th>
+														<th scope='col' class='text-center'>Amount</th>
+														<th scope='col' class='text-center' >Year</th>
+													</tr>
+												</thead>
+												<tbody>";
+
+											foreach ($conn->query($sql_2) as $row_2) {
+												echo "
+												<tr class='accordion-toggle collapsed text-center' id='accordion1' data-bs-toggle='collapse' data-bs-target='#collapseOne-$row_2[dues_id]' aria-expanded='true' aria-controls='collapseOne'>
+													<td class='expand-button text-center'></td>
+													<td>$row_2[month]</td>
+													<td style='color:green;'>$row_2[amount].00</td>
+													<td>$row_2[year]</td>
+												</tr>";
+											
 
 											
-									}
+												echo "
+												<tr class='hide-table-padding m-auto'>
+													<!--<td></td>-->
+													<td colspan='4'>
+														<div id='collapseOne-$row_2[dues_id]' class='collapse in p-3'>
+															<div class='col-12'>
+																<div class='row' id='drop1' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
+																	<label class='col-4' style='font-weight:bold;text-align: right;padding-left: 0;'>Dues ID:</label>
+																	<label class='col-5' style='text-align: left;' >$row_2[dues_id]</label>
+																</div>
+																<div class='row' id='drop1' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
+																	<label class='col-4' style='font-weight:bold;text-align: right;padding-left: 0;'>Date:</label>
+																	<label class='col-5' style='text-align: left;' >$row_2[dateofpayment]</label>
+																</div>
+																<div class='row' id='drop2' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
+																	<label  class='col-4' style='font-weight:bold;text-align: right;color:#003bb3;'>Admin ID:</label>
+																	<label  class='col-5' style='text-align: left;color:#003bb3;'>$row_2[adminid]</label>
+																</div>
+																<div class='row' id='drop2' style='display:flex;justify-content: center;align-items: center;flex-direction: row;'>
+																	<label class='col-4' style='font-weight:bold;text-align: right;color:#003bb3;'>Admin:</label>
+																	<label class='col-5' style='text-align: left;color:#003bb3;'>$row_2[approvedby]</label>
+																</div>
+																
+															</div>
+														</div>
+													</td>
+												</tr>";
+											}
 
-								echo "
-								</tbody>
-							</table>
-						</div>
+											
+														//If Member does not have any Due payments made
+													if (($result_2->num_rows == 0)) {
+														echo '
+														
+														<script>
+
+															$(".table-heads").hide();
+															$(".modal-footer").hide();
+															$(".chooseyear").hide();
+															$(".modalhead").css("background-color","red");
+															$(".modaltext").show().html("No Dues Payments Made");
+	
+														</script>
+														
+														';
+	
+												}
+
+											echo "
+											</tbody>
+										</table>
+									</div>
 						
 
 										</div>
 
-									<!--Modal Footer -->
 									
 									</div>
 									</div>
 								</div>
 								</div>  
 							</div>
+							<!--Modal Footer -->
+							<div class='modal-footer'>
+
+								<div class='col-12' style='display:flex;justify-content:center;align-items:center'>
+								
+									<button id='stepone' type='button' class='btn btn-outline'
+										style='background-color:white;color:green;border-color: green; visibility:hidden;'>Payed</button>
+									<button id='stepone' type='button' class='btn btn-primary pay_2'
+										style='background-color:green;border-color: green;'>Total Dues Paid </button>
+								</div>
+							
+							</div>
 						</div>
 
 						</div>
 						
-						</div>		
+						</div>	
+						
 						";
 
 
